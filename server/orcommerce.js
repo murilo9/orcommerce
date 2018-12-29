e8a39c2d6b48ff1a929c2af6826f2202d60b8b78
Murilo Henrique <murilomatias@alunos.utfpr.edu.br>
2018-12-29 09:59:07 -0200

    Router de session implementado no servidor; estilo adicionado

--- server/orcommerce.js
+++ server/orcommerce.js
@@ -34,5 +34,50 @@ app.listen(8888, function () {
     });
 });
 
-/* Router de Sesion */
+/* Router de Session */
 app.route('session')
+.get(function(req, res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    console.log('Recebeu GET session');
+    //Coleta os dados da request
+    var sessionId = req.body.sessionId;
+    var email = req.body.email;
+    var now = new Date();
+    var found = 0;
+    session.forEach(function(val, i){       //Percorre o array de session ids
+        if(val.id == sessionId && val.email == email && val.expire < now){
+            found = 1;
+        }
+    });
+    if(!found)      //Caso a session não seja válida ou tenha expirado
+        res.status(404);        //Status 404 not found
+    res.end();
+})
+.post(function(req, res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    console.log('Recebeu POST session');
+    //Coleta os dados da request:
+    var email = req.body.email;
+    var senha = req.body.senha;
+    //Faz a consulta ao BD:
+    var sql = "SELECT stSenha FROM tbUsuarios WHERE stEmail = '"+email+"'";
+    pool.query(sql, function(err, result, fields){
+        if(err){
+            console.log(err);
+            res.status(500);
+        }else{
+            if(result[0].stSenha == senha){         //Caso o login seja válido
+                var id = Math.floor(Math.random()*99999999);     //Gera o session id
+                var expireDate = new Date();
+                expireDate.setMinutes = expireDate.getDate + 20;    //Adiciona 20 min ao session expire time
+                //Armazena esta session id com o expire date e o email no array de sessions do server:
+                session.push({id: id, expire: expireDate, email: email});
+                res.send({sessionId: id});      //Envia a response com a session id
+                console.log('login efetuado com sucesso. Session id: '+ id);
+            }else{      //Caso o login seja inválido
+                res.status(401);    //Status 400 unauthorized
+            }
+        }
+        res.end();
+    });
+});
