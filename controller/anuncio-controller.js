1413a3bfafbcd2435eeb99fee1ef03b0843f6fe0
Murilo Henrique <murilomatias@alunos.utfpr.edu.br>
2019-01-15 06:52:10 -0200

    Implementado chat na página do anúncio

--- controller/anuncio-controller.js
+++ controller/anuncio-controller.js
@@ -1,8 +1,18 @@
 var anuncioController = new Vue({
     el: '#anuncioDetalhe',
     data: {
-        anuncio: {}
+        chatRequest: {      //Objeto que será enviado na request ao requisitar o chat
+            sessionId: Cookies.get('sessionId'), email: Cookies.get('email'), 
+            anuncioId: '', funcao: 'select', ator: 'cliente', chatId: ''
+        },
+        anuncio: {      //Objeto que contém os dados do anúncio que está sendo exibido nesta página
+            //(recebe a response do servidor)
+        },
+        chat: {
+            id: '', ready: false, time: ''
+        }
     },
+
     methods: {
         getData: function(data){    //Retorna uma string formatada de uma data
             var anuncioData = new Date(data);
@@ -12,8 +22,83 @@ var anuncioController = new Vue({
             var hora = anuncioData.getHours();
             var minuto = anuncioData.getMinutes();
             return dia+'/'+mes+'/'+ano+' às '+hora+':'+minuto;
+        },
+
+        iniciarChat: function(){    //Pega os dados do chat do servidor
+            var self = this;    //Variável self para referenciar data
+            if(Cookies.get('logged') == 'true'){    //Verifica se o usuário está logado
+                self.chatRequest.anuncioId = self.anuncio.id;   //Coloca a id do anuncio em chatRequest
+                //Faz a requisição da id da conversa pro servidor:
+                $.ajax({
+                    url: 'http://localhost:8888/chat',
+                    method: 'post',
+                    data: self.chatRequest,
+                    statusCode: {
+                        500: function(){
+                            alert('Erro no servidor, tente novamente mais tarde.');
+                        },
+                        401: function(){
+                            alert('Sua sessão expirou, faça login novamente.');
+                            Cookies.set('logged', 'false');
+                        },
+                        405: function(){
+                            alert('Você não pode abrir o chat em um anuncio que é seu.');
+                        }
+                    },
+                    success: function(res){
+                        self.chat.id = res.chatId;      //Pega a chat id:
+                        self.chat.ready = true;     //Ativa o chat
+                        self.chat.time = setInterval(function(){self.atualizarChat()},2000) //Ativa o atualziador de chat
+                    }
+                });
+            }else{      //Caso o usuário não esteja logado:
+                alert('Faça login para inicar uma conversa com o venedor.');
+            }
+        },
+
+        atualizarChat: function(){      //Atualiza o chat a cada 2s
+            //Lê o arquivo _chatId no servidor:
+            $.post("server/anuncios/_"+this.anuncio.id+"/chats/_"+this.chat.id+".html", '', function(res){
+                $('#chat-conteudo').html(res);      //Coloca a response no innerHTML do chat-conteudo
+                $('.mensagem-cliente').attr('style','float: right');    //Ajusta o estilo das mensagens
+            })
+        },
+
+        desativarChat: function(){   //Desativa o chat
+            this.chat.ready = false;        //Desativa o v-if da view
+            clearInterval(this.chat.time);      //Desativa o atualizador
+        },
+
+        enviarMensagem: function(){
+            var self = this;    //Variável self para referenciar data
+            this.chatRequest.funcao = 'update';     //Coloca função como update
+            this.chatRequest.chatId = this.chat.id;     //Coloca a chat id
+            this.chatRequest.mensagem = $('#chat-box').val();   //Coloca a mensagem digitada em chatRequest
+            $('#chat-box').val('');     //Limpa o chat box
+            //Envia a request para o servidor:
+            $.ajax({
+                url: 'http://localhost:8888/chat',
+                method: 'post',
+                data: self.chatRequest,
+                statusCode: {
+                    500: function(){
+                        alert('Erro no servidor. Tente novamente mais tarde.');
+                    },
+                    400: function(){
+                        alert('Erro interno. Tente novamente mais tarde.');
+                    },
+                    401: function(res){
+                        alert('Sua sessão expirou, faça login novamente.');
+                        Cookies.set('logged', 'false');
+                    }
+                },
+                success: function(res){
+                    self.atualizarChat();   //Atualiza o chat
+                }
+            });
         }
     },
+
     created: function(){
         var anuncioId = window.location.search.substring(4);    //Pega o id da query string
         var self = this;    //Variável self para referenciar data
@@ -32,3 +117,4 @@ var anuncioController = new Vue({
         });
     }
 });
+
--- server/orcommerce.js
+++ server/orcommerce.js
@@ -438,8 +438,10 @@ app.route('/anuncio/foto')
             return 0;
         }
         var arquivoNome = files.iFoto.name;     //Pega o nome do arquivo
-        var serverPath = 'anuncios/_'+anuncioId+'/';     //define a pasta temporária no servidor
-        fs.mkdirSync(serverPath);    //Cria a pasta temporária no servidor
+        var serverPath = 'anuncios/_'+anuncioId+'/';     //define a pasta do anuncio no servidor
+        fs.mkdirSync(serverPath);    //Cria a pasta do anuncio no servidor
+        var serverChatPath = serverPath+'chats/';   //Define a pasta onde ficarão os arquivos de chat
+        fs.mkdirSync(serverPath);    //Cria a pasta de chats do anuncio no servidor
         var oldPath = files.iFoto.path;     //Caminho de origem do arquivo
         var newPath = serverPath + arquivoNome;     //Caminho de destino do arquivo = pasta no servidor + nome do arquivo
         fs.rename(oldPath, newPath, function(err){      //Tenta mover o arquivo da origem pro destino
@@ -463,6 +465,113 @@ app.route('/anuncio/foto')
             }
         })
     });
+});
+
+/* Router de chat */
+
+app.route('/chat')
+.post(function(req, res){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    console.log('Recebeu POST chat');
+    var funcao = req.body.funcao;
+    switch(funcao){
+        case 'select':
+        console.log(req.body);
+            //Coleta os dados da request:
+            var sessionId = req.body.sessionId;
+            var email = req.body.email;
+            var anuncioId = req.body.anuncioId;
+            //Verifica se a session id é válida:
+            if(!sessionVerif(sessionId, email)){
+                res.status(401)     //Status 401 unauthorized
+                res.end();
+            }else{
+                //Verifica se o usuário não é o próprio dono do anúncio:
+                var sql = "SELECT * FROM tbAnuncios WHERE stDono='"+email+"' && itId="+anuncioId;
+                pool.query(sql, function(err, result, fidels){
+                    if(err){
+                        console.log(err);
+                        res.status(500);
+                        res.end();
+                    }else{
+                        if(result.length > 0){      //Caso haja algum resultado, então o usuário é o dono
+                            res.status(405);    //Status 405 not allowed
+                            res.end();
+                        }else{      //Caso não haja resultado, prossegue:
+                            //Verifica se uma conversa com este usuário/anúncio existe:
+                            var sql2 = "SELECT * FROM tbChat WHERE stUsuario='"+email+"' && itAnuncio="+anuncioId;
+                            pool.query(sql2, function(err, result, fields){
+                                if(err){
+                                    console.log(err);
+                                    res.status(500);
+                                    res.end();
+                                }else{
+                                    if(result.length > 0){      //Se a conversa existir
+                                        res.send({chatId: result[0].itId});     //Envia a id do chat para acessar o arquivo
+                                    }else{      //Se a conversa não existir, cria a conversa:
+                                        var id = Math.floor(Math.random()*999999999);   //Gera a chat id
+                                        var sql3 = "INSERT INTO tbChat VALUES("+id+",'"+email+"',"+anuncioId+")";
+                                        pool.query(sql3, function(err, result, fields){
+                                            if(err){
+                                                console.log(err);
+                                                res.status(500);
+                                                res.end();
+                                            }else{      //Caso o insert tenha sido bem-sucedido
+                                                //Cria o arquivo do chat no servidor:
+                                                fs.appendFileSync('anuncios/_'+anuncioId+'/chats/_'+id+'.html', '');
+                                                res.send({chatId: id});     //Envia a id na response
+                                            }
+                                        });
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+            break;
+
+        case 'update':
+            //Coleta os dados da request:
+            var sessionId = req.body.sessionId;
+            var email = req.body.email;
+            var anuncioId = req.body.anuncioId;
+            var chatId = req.body.chatId;
+            var ator = req.body.ator;
+            var mensagem = '';
+            //Configura o ator da mensagem (vendedor ou cliente)
+            if(ator == 'cliente')
+                mensagem = "<div class='mensagem-cliente'>";
+            else if(ator == 'vendedor')
+                mensagem = "<div class='mensagem-vendedor'>";
+            else{   //Caso o ator não esteja definido
+                res.status(400);    //Status 400 bad request
+                res.end();
+                return 0;
+            }
+            //Verifica se a session id é válida:
+            if(!sessionVerif(sessionId, email)){
+                res.status(401)     //Status 401 unauthorized
+                res.end();
+            }else{
+                //Grava a mensagem no arquivo de chat:
+                mensagem += req.body.mensagem;
+                mensagem += '</div>';
+                fs.appendFileSync('anuncios/_'+anuncioId+'/chats/_'+chatId+'.html', mensagem, function(err){
+                    if(err){
+                        res.status(500);
+                        res.end();
+                    }else{
+                        res.end();
+                    }
+                });
+            }
+            break;
+
+        default:
+            res.status(404);
+            res.end();
+    }
 });
 
 function sessionVerif(sessionId, email){
